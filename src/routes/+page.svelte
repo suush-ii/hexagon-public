a72d6c76fe111322cca1a0b2b46b083c0bcec7d1
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2023-09-21 23:02:49 -0600

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,52 @@
+<div
+	class="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+>
+	<div
+		class="relative hidden h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex"
+	>
+		<div
+			class="absolute inset-0 bg-cover"
+			style="
+				background-image:
+					url(https://images.unsplash.com/photo-1513569771920-c9e1d31714af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80);"
+		/>
+		<div class="relative z-20 flex items-center text-lg font-medium">
+			<!-- <Command class="mr-2 h-6 w-6" /> -->
+			Hexagon
+		</div>
+	</div>
+	<div class="lg:p-8">
+		<div
+			class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]"
+		>
+			<div class="flex flex-col space-y-2 text-center">
+				<h1 class="text-2xl font-semibold tracking-tight">
+					Create an account
+				</h1>
+				<p class="text-sm text-muted-foreground">
+					Enter your email below to create your account
+				</p>
+			</div>
+
+            
+
+			<p class="px-8 text-center text-sm text-muted-foreground">
+				By clicking continue, you agree to our{" "}
+				<a
+					href="/terms"
+					class="underline underline-offset-4 hover:text-primary"
+				>
+					Terms of Service
+				</a>{" "}
+				and{" "}
+				<a
+					href="/privacy"
+					class="underline underline-offset-4 hover:text-primary"
+				>
+					Privacy Policy
+				</a>
+				.
+			</p>
+		</div>
+	</div>
+</div>
