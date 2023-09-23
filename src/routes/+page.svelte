e23da593d05ab653deb1edcb7e289019f45d7fcb
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2023-09-23 17:00:47 -0600

    signup page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,104 @@
-<div
-	class="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
->
-	<div
-		class="relative hidden h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex"
-	>
-		<div
-			class="absolute inset-0 bg-cover"
-			style="
-				background-image:
-					url(https://images.unsplash.com/photo-1513569771920-c9e1d31714af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80);"
-		/>
-		<div class="relative z-20 flex items-center text-lg font-medium">
-			<!-- <Command class="mr-2 h-6 w-6" /> -->
-			Hexagon
-		</div>
-	</div>
-	<div class="lg:p-8">
+<script lang="ts">
+  import { Button } from "$src/components/ui/button";
+  import * as Card from "$src/components/ui/card";
+  import * as Select from "$src/components/ui/select";
+  import { Input } from "$src/components/ui/input";
+  import { Label } from "$src/components/ui/label";
+  import { Loader2 } from "lucide-svelte";
+
+  let isLoading = false;
+  
+	async function onSubmit() {
+		console.log('wtf')
+		isLoading = true;
+
+		setTimeout(() => {
+			isLoading = false;
+		}, 3000);
+	}
+
+</script>
+	<div class="flex h-screen">
+		<Button class="absolute right-5 top-5">
+			Log In</Button>
+	
 		<div
 			class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]"
 		>
+
+		<div class="relative mx-auto font-bold z-20 flex items-center text-5xl">
+			Hexagon
+		</div>
+
 			<div class="flex flex-col space-y-2 text-center">
 				<h1 class="text-2xl font-semibold tracking-tight">
 					Create an account
 				</h1>
 				<p class="text-sm text-muted-foreground">
-					Enter your email below to create your account
+					Enter your details below to create your account
 				</p>
 			</div>
 
+
+			<Card.Root>
+				<form on:submit|preventDefault={onSubmit}>
+				<Card.Content>
+						<div class="grid w-full items-center gap-4">
+						  <div class="flex flex-col space-y-1.5">
+							<Label for="username">Username</Label>
+							<Input id="username" disabled={isLoading} placeholder="(3-20 Characters, no spaces)" />
+						  </div>
+						  <div class="flex flex-col space-y-1.5">
+							<Label for="password">Password</Label>
+							<Input id="password" disabled={isLoading} type="password" placeholder="(Unique)" />
+						  </div>
+						  <div class="flex flex-col space-y-1.5">
+							<Label for="gender">Gender</Label>
+							<Select.Root disabled={isLoading}>
+							  <Select.Trigger id="gender">
+								<Select.Value placeholder="Select" />
+							  </Select.Trigger>
+							  <Select.Content>
+
+								<Select.Item value="male" label="Male"
+								>{"Male"}</Select.Item
+							  	>
+
+								<Select.Item value="female" label="Female"
+								>{"Female"}</Select.Item
+								>
+
+								
+								<Select.Item value="nonbinary" label="None"
+								>{"None"}</Select.Item
+								>
+
+							  </Select.Content>
+							</Select.Root>
+						  </div>
+						</div>
+					  
+				</Card.Content>
+				<Card.Footer>
+					<Button disabled={isLoading} type="submit" class="w-full">
+						{#if isLoading}
+						<Loader2 class="mr-2 h-4 w-4 animate-spin" />
+						{/if}
+						Sign Up!</Button>
+				</Card.Footer>
+				</form>
+			  </Card.Root>
             
 
 			<p class="px-8 text-center text-sm text-muted-foreground">
-				By clicking continue, you agree to our{" "}
+				By clicking continue, you agree to our
 				<a
 					href="/terms"
 					class="underline underline-offset-4 hover:text-primary"
 				>
 					Terms of Service
-				</a>{" "}
-				and{" "}
+				</a>
+				and
 				<a
 					href="/privacy"
 					class="underline underline-offset-4 hover:text-primary"
@@ -49,4 +109,3 @@
 			</p>
 		</div>
 	</div>
-</div>
