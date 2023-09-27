04c38f2b47b76ce84dc3cbc99ac28f21212fbb94
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2023-09-26 22:31:47 -0600

    underscore only

--- src/lib/schemas/signupschema.ts
+++ src/lib/schemas/signupschema.ts
@@ -1,10 +1,45 @@
 import { z } from 'zod';
 
+function isAlphaNumeric(str: string) {
+	var code, i, len
+	let underscores = 0
+
+	for (i = 0, len = str.length; i < len; i++) {
+		code = str.charCodeAt(i)
+
+		if (
+			!(code > 47 && code < 58) && // numeric (0-9)
+			!(code > 64 && code < 91) && // upper alpha (A-Z)
+			!(code === 95) && // underscore
+			!(code > 96 && code < 123)
+		) {
+			// lower alpha (a-z)
+			return false
+		}
+	}
+
+	for (i = 0, len = str.length; i < len; i++) {
+		code = str.charCodeAt(i)
+
+		if (code === 95){
+			underscores += 1
+		}
+
+	}
+
+	if (underscores > 1){
+		return false
+	}
+
+	return true
+}
+
 export const formSchema = z.object({
 	username: z
 		.string({ required_error: 'Username required.' })
 		.min(3, { message: 'Username must be at least 3 characters!' })
-		.max(20, { message: "Username can't be over 20 characters!" }),
+		.max(20, { message: "Username can't be over 20 characters!" })
+		.refine((value) => isAlphaNumeric(value), "Only special characters allowed are one underscore."),
 	password: z.string({ required_error: 'Password required.' }).min(1).max(100),
 	key: z.string({ required_error: 'Key required.' }).min(1).max(100),
 	gender: z.enum(['male', 'female', 'nonbinary']).default('nonbinary')
