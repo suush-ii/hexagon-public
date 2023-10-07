c7e0c5c892d7f59156cc2b8916d74edbfba115a5
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2023-10-06 22:55:02 -0600

    home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,15 +2,33 @@
 	import type { PageData } from "./$types";
 
     export let data: PageData;
+
+    import * as Avatar from "$src/components/ui/avatar";
+
+    import { HeartCrack } from 'lucide-svelte';
+
 </script>
 
-<div class="container p-8 flex flex-col gap-y-24">
+<div class="container p-8 flex flex-col">
 
     <h1 class="text-5xl leading-none tracking-tight">{data.welcomeMessage}, {data.session.username}...</h1>
 
-    <h1 class="text-3xl">Friends (0)</h1>
+    <h1 class="text-3xl pt-24">Friends (0)</h1>
 
-    <div class="supports-backdrop-blur:bg-background/60 w-full border-b bg-muted-foreground/5 shadow-sm backdrop-blur flex flex-row">
+    <div class="supports-backdrop-blur:bg-background/60 w-full border-b bg-muted-foreground/5 shadow-sm backdrop-blur flex flex-row p-4 gap-x-4 select-none">
+
+        <div class="mx-auto flex flex-col w-full gap-y-4">
+            <HeartCrack class="w-24 h-24 mx-auto" strokeWidth={1.2} />
+            <h5 class="text-center mx-auto ">There seems to be no one here... 😕</h5>
+        </div>
+
+        <!--<div>
+        <Avatar.Root class="w-24 h-24">
+            <Avatar.Image src="https://avatars.githubusercontent.com/u/54828677?v=4" alt="@sushidesigner" />
+            <Avatar.Fallback>SU</Avatar.Fallback>
+          </Avatar.Root>
+          <p class="truncate">SushiDesigner</p>
+        </div>-->
 
     </div>
 </div>
