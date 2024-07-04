b025ce852498945bc970ace164e354c285fd6bd3
Sushi <54828677+SushiDesigner@users.noreply.github.com>
2024-07-04 17:53:02 -0600

    idk whats happening

--- src/routes/(studio)/login/RequestAuth.ashx/+server.ts
+++
@@ -1,5 +0,0 @@
-import { text, type RequestHandler } from '@sveltejs/kit'
-
-export const GET: RequestHandler = async () => {
-	return text('http://hexagon.pw/login/Negotiate.ashx?suggest=ROBLO') // 1: logged in null: logged out
-}
