ddd636ae150892d1c538ae3c9bf9b8e45f1f1d59
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2024-08-06 15:46:13 -0600

    update emptycard

--- src/components/emptyCard.svelte
+++ src/components/emptyCard.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
-	import { HeartCrack } from 'lucide-svelte'
+	import { CircleHelp } from 'lucide-svelte'
 
 	export let hideDefault = false
 	import { page } from '$app/stores'
+	import { onMount } from 'svelte'
+
+	let loaded = false
+
+	onMount(async () => {
+		await new Promise((resolve) => setTimeout(resolve, 100))
+
+		loaded = true
+	})
 </script>
 
 <div class="mx-auto flex flex-col w-full gap-y-4 select-none" {...$$restProps}>
-	<HeartCrack class="w-24 h-24 mx-auto" strokeWidth={1.2} />
+	<CircleHelp
+		class="w-24 h-24 mx-auto transition-all duration-1000 ease-in-out {loaded
+			? 'rotate-0 opacity-100 scale-100'
+			: 'rotate-[-200deg] opacity-80 scale-90'}"
+		strokeWidth={1.5}
+	/>
 	{#if !hideDefault}<h5 class="text-center mx-auto select-none">
 			{$page.data.t('emptyCard.empty')}
 		</h5>{/if}
