6383bdaa13b4581079393f9fc719bc03b63a1895
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2024-08-14 19:33:47 -0600

    show favorites on userpage

--- src/lib/server/games/getImage.ts
+++ src/lib/server/games/getImage.ts
@@ -1,7 +1,14 @@
 import { sql } from 'drizzle-orm'
 import { assetTable } from '../schema'
+import type { PgTableWithColumns } from 'drizzle-orm/pg-core'
 
 export const imageSql =
 	sql`CASE WHEN ${assetTable.moderationstate} = 'approved' THEN ${assetTable.simpleasseturl} ELSE NULL END`.as(
 		'simpleasseturl'
 	)
+
+export function aliasedimageSql(alias: PgTableWithColumns<any>) {
+	return sql`CASE WHEN ${alias.moderationstate} = 'approved' THEN ${alias.simpleasseturl} ELSE NULL END`.as(
+		'simpleasseturl'
+	)
+}
