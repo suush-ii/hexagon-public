b47da7b1885a6df3e6cf81ab27db9c7ee0161696
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2024-09-09 21:55:47 -0600

    fix

--- src/routes/(rcc)/verify-player/ip/+server.ts
+++ src/routes/(rcc)/verify-player/ip/+server.ts
@@ -1,14 +1,12 @@
 import { db } from '$lib/server/db'
 import { usersTable } from '$lib/server/schema'
-import { json, type RequestHandler, text } from '@sveltejs/kit'
+import { json, type RequestHandler } from '@sveltejs/kit'
 import { eq, or, and, isNull } from 'drizzle-orm'
 
 export const POST: RequestHandler = async ({ request }) => {
 	const body = await request.json()
 
 	let { ip } = body
-
-	ip = ip.split(':')[0]
 
 	const result = await db
 		.select({ lastip: usersTable.lastip })
