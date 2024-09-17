ca1d92251f13d775643d8ad4b4b99f164fd1548a
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2024-09-16 18:45:47 -0600

    Update +server.ts

--- src/routes/(main)/api/avatar/redraw/+server.ts
+++ src/routes/(main)/api/avatar/redraw/+server.ts
@@ -1,8 +1,8 @@
 import { error, json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 import { db } from '$src/lib/server/db'
-import { usersTable } from '$src/lib/server/schema'
-import { eq } from 'drizzle-orm'
+import { jobsTable, usersTable } from '$src/lib/server/schema'
+import { and, eq } from 'drizzle-orm'
 
 export const POST: RequestHandler = async ({ locals }) => {
 	const user = locals.user
@@ -19,6 +19,16 @@ export const POST: RequestHandler = async ({ locals }) => {
 			_3dmanifest: null
 		})
 		.where(eq(usersTable.userid, user.userid))
+
+	await db
+		.delete(jobsTable)
+		.where(
+			and(
+				eq(jobsTable.associatedid, user.userid),
+				eq(jobsTable.type, 'render'),
+				eq(jobsTable.rendertype, 'user')
+			)
+		)
 
 	return json({ success: true, message: '', data: {} })
 }
