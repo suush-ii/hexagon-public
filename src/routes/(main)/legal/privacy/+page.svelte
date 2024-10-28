b0f3292949416a762878bb2825402a29658afe56
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2024-10-27 21:34:43 -0600

    privacy policy
    this should of been done a long time ago oops..

---
+++ src/routes/(main)/legal/privacy/+page.svelte
@@ -0,0 +1,21 @@
+<div class="container p-4 flex flex-col gap-y-4">
+	<h1 class="text-4xl leading-none tracking-tight font-semibold">Privacy Policy</h1>
+	<p class="text-lg">At Hexagon, we take your privacy seriously. Here's what we store.</p>
+	<ul class="list-disc text-base space-y-4">
+		<li><span class="font-semibold">IP Address.</span> I promise we won't share it.</li>
+		<li class="font-semibold">HWID</li>
+		<li class="font-semibold">Your hashed password.</li>
+		<li>
+			<span class="font-semibold"
+				>We use <a class="hover:underline" href="https://umami.is/">umami</a></span
+			> an open source and privacy friendly analytics collector.
+		</li>
+	</ul>
+
+	<p>We will never share your info with third parties.</p>
+
+	<p>
+		These terms may change at any time, with or without notice. We expect you to check these every
+		month or so.
+	</p>
+</div>
