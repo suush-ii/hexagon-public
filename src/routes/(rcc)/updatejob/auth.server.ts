849ea786ea3c1114767001fcbd3fe23f55ccd19a
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2024-11-14 16:50:28 -0700

    make game assets and api key overall more secure

--- src/routes/(rcc)/updatejob/auth.server.ts
+++ src/routes/(rcc)/updatejob/auth.server.ts
@@ -1,5 +1,9 @@
 import { type Handle, error } from '@sveltejs/kit'
 import { env } from '$env/dynamic/private'
+import * as jose from 'jose'
+import { db } from '$lib/server/db'
+import { eq } from 'drizzle-orm'
+import { jobsTable } from '$lib/server/schema'
 
 export const rccAuth = (async ({ event, resolve }) => {
 	const pathname = event.url.pathname.toLowerCase()
@@ -21,12 +25,31 @@ export const rccAuth = (async ({ event, resolve }) => {
 	) {
 		const accessKey = event.url.searchParams.get('apikey') || event.request.headers.get('accessKey')
 
-		if (!accessKey || (env.RCC_ACCESS_KEY as string) != accessKey) {
-			return error(403, {
-				success: false,
-				message: 'Invalid session.',
-				data: {}
+		try {
+			const { payload } = await jose.jwtVerify(
+				accessKey ?? '',
+				new TextEncoder().encode(env.RCC_ACCESS_KEY as string)
+			)
+
+			const job = await db.query.jobsTable.findFirst({
+				where: eq(jobsTable.jobid, (payload?.jobid ?? '') as string)
 			})
+
+			if (!job) {
+				return error(403, {
+					success: false,
+					message: 'Invalid session.',
+					data: {}
+				})
+			}
+		} catch {
+			if (!accessKey || (env.RCC_ACCESS_KEY as string) != accessKey) {
+				return error(403, {
+					success: false,
+					message: 'Invalid session.',
+					data: {}
+				})
+			}
 		}
 	}
 
