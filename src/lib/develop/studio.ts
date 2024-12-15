0592435e8aac8eb8598d8ae3d6cfbd9039a5403d
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2024-12-15 13:49:48 -0700

    Open studio without games

--- src/lib/develop/studio.ts
+++ src/lib/develop/studio.ts
@@ -1,12 +1,20 @@
 import DownloadModal from '$src/components/downloadModal.svelte'
 
-export function launchStudio(
+export function launchStudioScript(
 	placeid: number,
 	authBearer: string,
 	baseurl: string,
 	downloadModal: DownloadModal
 ) {
 	const uri = `hexagon-studio:1+launchmode:ide+gameinfo:${authBearer}+script:${encodeURIComponent(`http://www.${baseurl}/Game/edit.ashx?PlaceID=${placeid}`)}`
+
+	downloadModal.open(uri)
+
+	document.location = uri
+}
+
+export function launchStudio(authBearer: string, downloadModal: DownloadModal) {
+	const uri = `hexagon-studio:1+launchmode:ide+gameinfo:${authBearer}`
 
 	downloadModal.open(uri)
 
