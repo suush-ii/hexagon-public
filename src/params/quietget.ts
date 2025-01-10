8fbc2a25e133ebfcfb8bdfe27d09a4754652beaa
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2025-01-09 20:09:03 -0700

    lil android stuff

--- src/params/quietget.ts
+++ src/params/quietget.ts
@@ -1,4 +1,7 @@
 import type { ParamMatcher } from '@sveltejs/kit'
 
 export const match: ParamMatcher = (param) =>
-	param === 'ClientAppSettings' || param === 'ClientSharedSettings' || param === 'RCCService'
+	param === 'ClientAppSettings' ||
+	param === 'ClientSharedSettings' ||
+	param === 'RCCService' ||
+	param === 'AndroidAppSettings'
