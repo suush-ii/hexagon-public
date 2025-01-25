5dd61b041599d803d80bbe61f11a4a6e28ab4a19
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2025-01-25 12:12:37 -0700

    separate these

--- src/routes/(rbx)/[g]ame/gameserver.ashx/+server.ts
+++ src/routes/(rbx)/[g]ame/gameserver.ashx/+server.ts
@@ -1,11 +1,13 @@
 import { error, text, type RequestHandler } from '@sveltejs/kit'
 import { env } from '$env/dynamic/private'
 import script from './gameserver.lua?raw'
+import script2013 from './gameserver.lua?raw'
 import scriptDefault from './gameserver_studio.lua?raw'
 import { createSign } from 'node:crypto'
 import * as jose from 'jose'
 
 const scriptNew: string = script.replaceAll('roblox.com', env.BASE_URL as string)
+const script2013New: string = script2013.replaceAll('roblox.com', env.BASE_URL as string)
 
 export const GET: RequestHandler = async ({ url }) => {
 	const accessKey = url.searchParams.get('apikey')
@@ -20,6 +22,19 @@ export const GET: RequestHandler = async ({ url }) => {
 
 			return text('--rbxsig%' + signature + '%\r\n' + scriptDefault)
 		}
+	}
+
+	if (url.searchParams.get('version') === '2013') {
+		const scriptNew2013Args = script2013New.replace(
+			'{1}',
+			`"apikey=${url.searchParams.get('apikey')}", ${url.searchParams.get('placeId')}, ${url.searchParams.get('port')}, "${url.searchParams.get('jobId')}", ${url.searchParams.get('maxPlayers')}`
+		)
+
+		const sign = createSign('SHA1')
+		sign.update('\r\n' + scriptNew2013Args)
+		const signature = sign.sign(env.CLIENT_PRIVATE_KEY as string, 'base64')
+
+		return text('--rbxsig%' + signature + '%\r\n' + scriptNew2013Args)
 	}
 
 	const scriptNewArgs = scriptNew.replace(
