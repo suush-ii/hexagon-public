9fda7279b24233a22b7ae5f3789c0b456822784c
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2025-02-01 14:21:18 -0700

    sysstats

--- src/routes/(rbx)/[g]ame/PlaceSpecificScript.ashx/placespecificscript.lua
+++ src/routes/(rbx)/[g]ame/PlaceSpecificScript.ashx/placespecificscript.lua
@@ -2,6 +2,7 @@ local access = "{}"
 
 game:GetService("Players"):SetSaveDataUrl("http://www.roblox.com/Persistence/SetBlob.ashx?" .. access .. "&placeid=" .. {placeId} .. "&userid=%d")
 game:GetService("Players"):SetLoadDataUrl("http://www.roblox.com/Persistence/GetBlobUrl.ashx?" .. access .. "&placeid=" .. {placeId} .. "&userid=%d")
+pcall(function() game:GetService("Players"):SetSysStatsUrl("http://www.roblox.com/AbuseReport/Client.ashx?".. access .. "&placeid=" .. {placeId} .. "&jobid=" .. game.JobId) end)
 
 game:GetService("Players").PlayerAdded:connectFirst(function(player)
     player:LoadData()
