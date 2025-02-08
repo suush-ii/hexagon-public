28091658ae33f2af2760195d1e4de65c39892561
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2025-02-07 19:17:44 -0700

    wa

--- src/routes/(rbx)/mobileapi/login/+server.ts
+++ src/routes/(rbx)/mobileapi/login/+server.ts
@@ -8,7 +8,13 @@ import { db } from '$src/lib/server/db'
 import { eq } from 'drizzle-orm'
 import { usersTable } from '$src/lib/server/schema'
 import { s3Url } from '$src/stores'
+import { RateLimiter } from 'sveltekit-rate-limiter/server'
+import { authenticator } from '@otplib/preset-default'
 const Key = 'thumbnails'
+
+const limiter = new RateLimiter({
+	IP: [1, '2s']
+})
 
 export const POST: RequestHandler = async (event) => {
 	const form = await superValidate(event, zod(formSchema))
@@ -18,7 +24,7 @@ export const POST: RequestHandler = async (event) => {
 			UserInfo: {}
 		})
 	}
-	const { username, password } = form.data
+	const { username, password, _2facode } = form.data
 
 	let session_new
 
@@ -28,6 +34,37 @@ export const POST: RequestHandler = async (event) => {
 			username.toString().toLowerCase(),
 			password.toString()
 		)
+
+		const userKey = await db.query.usersTable.findFirst({
+			where: eq(usersTable.id, user.userId),
+			columns: {
+				_2fasecret: true
+			}
+		})
+
+		if (userKey?._2fasecret) {
+			if (!_2facode) {
+				return json({
+					Status: 'MissingRequiredField',
+					UserInfo: {}
+				})
+			}
+
+			if (await limiter.isLimited(event)) {
+				return json({
+					Status: 'Ratelimit',
+					UserInfo: {}
+				})
+			}
+
+			if (!authenticator.check(_2facode.toString(), userKey._2fasecret)) {
+				return json({
+					Status: 'Invalid2FACode',
+					UserInfo: {}
+				})
+			}
+		}
+
 		const session = await auth.createSession({
 			userId: user.userId,
 			attributes: {}
