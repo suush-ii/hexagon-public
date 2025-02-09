809461670ee43485dd461956cf3009378944527a
SushiDesigner <54828677+SushiDesigner@users.noreply.github.com>
2025-02-08 22:49:18 -0700

    Update +server.ts

--- src/routes/(rcc)/GetAllowedMD5Hashes/+server.ts
+++ src/routes/(rcc)/GetAllowedMD5Hashes/+server.ts
@@ -7,7 +7,8 @@ export const GET: RequestHandler = async () => {
 			'7bdfdd1896e0a2c0d32e2d5fe90bf78a',
 			'4c17136f028ccdd610108fa1209b117e',
 			'a8c849c7dfed2e02969fd55301be7374',
-			'8e246cbf18054169c48d7f2c8e844230'
+			'8e246cbf18054169c48d7f2c8e844230',
+			'42bd28bb0701e1a2a870a4d38b0b2955'
 		]
 	})
 }
